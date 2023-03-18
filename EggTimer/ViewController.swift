1737765c22cc018628d1cecada6e26d47f42331c
jpibarra1130 <jpibarra1130@gmail.com>
2023-03-18 17:42:59 +0100

    Full project for the egg timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    var hardnessTypes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var timer = Timer()
+    var timerCurrentCount = 0
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var softButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    @IBOutlet weak var eggStackView: UIStackView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var currentHardness = 10
+    @IBAction func hardnessPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        currentHardness = hardnessTypes[hardness]!
+        self.timer = Timer.scheduledTimer(timeInterval: 1,
+                                          target: self,
+                                          selector: #selector(handleTimerExecution),
+                                          userInfo: nil,
+                                          repeats: true)
+        self.mainLabel.text = "Timer started for \(hardness)."
+        self.softButton.isEnabled = false
+        self.mediumButton.isEnabled = false
+        self.hardButton.isEnabled = false
+        self.eggStackView.layer.opacity = 0.5
+        self.progressBar.progress = 0.0
+    }
+    
+    @objc private func handleTimerExecution() {
+        if self.timerCurrentCount == currentHardness {
+            self.timer.invalidate() // invalidating timer
+            print("timer done... \(self.timerCurrentCount)")
+            self.timerCurrentCount = 0
+            self.mainLabel.text = "Cooked!"
+            self.softButton.isEnabled = true
+            self.mediumButton.isEnabled = true
+            self.hardButton.isEnabled = true
+            self.eggStackView.layer.opacity = 1
+            
+            
+        } else {
+            print("timer executed... \(self.timerCurrentCount)")
+            self.timerCurrentCount += 1
+            self.progressBar.progress = Float(self.timerCurrentCount) / Float(self.currentHardness)
+        }
+    }
 }
